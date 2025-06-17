c0a0dead2704ba59b34499b66066d655ad219ee1
anshu0908 <eklovyagauba94@gmail.com>
2025-06-17 10:32:57 +0530

    first commit

---
+++ PrimeHomz/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+import Home from './component/Home'
+import Listing from './component/Listing'
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+      <Listing/>
+    </div>
+  )
+}
+
+export default App
