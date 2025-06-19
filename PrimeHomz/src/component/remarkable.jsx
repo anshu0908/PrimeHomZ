d273af653971dd762635c6c28b5bf0c6a0d4fe71
Sanyog2230 <sanyogbiswal22@gmail.com>
2025-06-19 17:03:02 +0530

    home page changed,nav bar changed

--- PrimeHomz/src/component/remarkable.jsx
+++ PrimeHomz/src/component/remarkable.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Remarkable = () => {
   return (
-    <div className='h-[140vh] w-full bg-[#3B3E2D] pt-20 pb-20 px-19 flex flex-col gap-17'>
+    <div className='h-[140vh] w-full mt-7  bg-[#3B3E2D] pt-20 pb-20 px-19 flex flex-col gap-17'>
       <div className="upper-div w-full h-[40%]">
         <h1 className='text-white font-[geist] text-[6.2vw] leading-[7vw] tracking-tight font-medium'> <span className='font-[windsong] mr-3'>We</span> create remarkable neighbourhoods that encourage exploration and discovery. </h1>
       </div>
 
-      <div className="lower-div h-[50%] w-[85%] grid grid-cols-[57%_38%] gap-[5%] ">
+      <div className="lower-div h-[60vh] w-[78vw] grid grid-cols-[57%_38%] gap-[5%] ">
 
-      <div className="left h-full w-full bg-white border rounded-2xl">
-        <img className='h-full w-full object-cover' src="/remarkable image.webp" alt="" />
+      <div className="left h-[60vh] border-transparent border rounded-2xl">
+        <img className='h-full w-45vw object-cover' src="/remarkable image.webp" alt="" />
       </div>
 
       <div className="right space-y-4">
